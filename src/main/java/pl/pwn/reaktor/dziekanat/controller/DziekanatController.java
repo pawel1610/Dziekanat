9f7e082df6b95f62bcb373d37241ede773ab0b4c
pawel <pawelmaciaszek@gmail.com>
2019-03-17 13:41:55 +0100

    show/hide password button

--- src/main/java/pl/pwn/reaktor/dziekanat/controller/DziekanatController.java
+++ src/main/java/pl/pwn/reaktor/dziekanat/controller/DziekanatController.java
@@ -3,7 +3,10 @@ package pl.pwn.reaktor.dziekanat.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 
 public class DziekanatController {
 
@@ -20,6 +23,9 @@ public class DziekanatController {
     private TextField txtPassword;
 
     @FXML
+    private PasswordField pswFieldPassword;
+
+    @FXML
     private Button btnShow;
 
     @FXML
@@ -30,5 +36,42 @@ public class DziekanatController {
 
     @FXML
     private Button btnSignIn;
+
+    @FXML
+    void ContinueAsGuestEvent(MouseEvent event) {
+
+    }
+
+    @FXML
+    void LogInEvent(MouseEvent event) {
+
+    }
+
+    @FXML
+    void ShowPasswordEvent(MouseEvent event) {
+        if("SHOW".equalsIgnoreCase(btnShow.getText())){
+            txtPassword.setText(pswFieldPassword.getText());
+            txtPassword.setVisible(true);
+            pswFieldPassword.setVisible(false);
+            btnShow.setText("HIDE");
+
+        }else if("HIDE".equalsIgnoreCase(btnShow.getText())){
+            pswFieldPassword.setText(txtPassword.getText());
+            txtPassword.setVisible(false);
+            pswFieldPassword.setVisible(true);
+            btnShow.setText("SHOW");
+        }
+
+    }
+
+    @FXML
+    void SignInEvent(MouseEvent event) {
+
+    }
+
+    @FXML
+    void loginKeyAction(KeyEvent event) {
+
+    }
 
 }
