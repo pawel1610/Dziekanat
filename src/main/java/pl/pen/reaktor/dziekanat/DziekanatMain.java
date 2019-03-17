8beeb3ec4148358e386210cc84e5ee3be3236268
pawel <pawelmaciaszek@gmail.com>
2019-03-17 12:17:41 +0100

    initialize

---
+++ src/main/java/pl/pen/reaktor/dziekanat/DziekanatMain.java
@@ -0,0 +1,21 @@
+package pl.pen.reaktor.dziekanat;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class DziekanatMain extends Application {
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/dziekanatView.fxml")); // wskazujemy nasz widok FXML
+        primaryStage.setTitle("Dziekanat");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+
+    }
+}
---
+++ src/main/java/pl/pen/reaktor/dziekanat/controller/DziekanatController.java
@@ -0,0 +1,4 @@
+package pl.pen.reaktor.dziekanat.controller;
+
+public class DziekanatController {
+}
